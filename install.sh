97d7fbc14edf4509bb5c4c021dd21b69273c5db9
mebitek <mebitek@zohomail.eu>
2025-03-10 08:14:30 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -12,5 +12,10 @@ else
   ln -s  /data/dbus-tasmota-inverter/service /opt/victronenergy/service/dbus-tasmota-inverter
 fi
 
+wget https://raw.githubusercontent.com/victronenergy/velib_python/refs/heads/master/ve_utils.py
+wget https://raw.githubusercontent.com/victronenergy/velib_python/refs/heads/master/vedbus.py
+
 chmod 755 /data/dbus-tasmota-inverter/dbus-tasmota-inverter.py
+chmod 755 /data/dbus-tasmota-inverter/service/run
+chmod 755 /data/dbus-tasmota-inverter/kill_me.sh
 chmod 755 /data/dbus-tasmota-inverter/uninstall.sh
