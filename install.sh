971c2f46763009dbfa95144e7a9d7b92acd146ac
mebitek <mebitek@zohomail.eu>
2025-03-12 15:18:25 +0100

    add support for SetupHelper v6.0

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 wget -q https://raw.githubusercontent.com/victronenergy/velib_python/refs/heads/master/ve_utils.py -O ve_utils.py
 wget -q https://raw.githubusercontent.com/victronenergy/velib_python/refs/heads/master/vedbus.py -O vedbus.py
-
-chmod 755 /data/dbus-tasmota-inverter/dbus-tasmota-inverter.py
-chmod 755 /data/dbus-tasmota-inverter/service/run
-chmod 755 /data/dbus-tasmota-inverter/kill_me.sh
-chmod 755 /data/dbus-tasmota-inverter/uninstall.sh
--- kill_me.sh
+++ kill_me.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kill $(pgrep -f 'dbus-tasmota-inverter')
+kill $(pgrep -f 'TasmotaInverter')
