8ec5af287284e196d11cacc06a0d2199aaa8effc
mebitek <mebitek@zohomail.eu>
2025-03-12 14:48:22 +0100

    add support for SetupHelper v6.0

--- install.sh
+++ install.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-if [[ -L /service/dbus-tasmota-inverter ]]; then
-  echo "Service /service already exists"
-else
-  ln -s /data/dbus-tasmota-inverter/service /service/dbus-tasmota-inverter
-fi
-
-if [[ -L /opt/victronenergy/service/dbus-tasmota-inverter ]]; then
-  echo "Service /opt/victronenergy/service/ already exists"
-else
-  ln -s  /data/dbus-tasmota-inverter/service /opt/victronenergy/service/dbus-tasmota-inverter
-fi
-
 wget -q https://raw.githubusercontent.com/victronenergy/velib_python/refs/heads/master/ve_utils.py -O ve_utils.py
 wget -q https://raw.githubusercontent.com/victronenergy/velib_python/refs/heads/master/vedbus.py -O vedbus.py
 
